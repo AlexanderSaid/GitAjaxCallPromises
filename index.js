dc7eba9fd21e79b2c9ed83f9d5f7321a29a195f3
HalimSd <a.alhalim94@gmail.com>
2017-08-13 01:33:20 +0200

    some changes to the code ... and yeah i know it's a bad descreption

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-function Request () {}
-function RenderList () {}
-function GetHtmlRepoList () {}
-function GetHtmlMemberList () {}
+function Request(requestRL, cb) {
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest;
+    function processRequest() {
+      if (req.readyState === 4 && req.status === 200) {
+        resolve(JSON.parse(req.response));
+      } else if (req.readyState === 4 && req.status !== 200) {
+        reject(req, req.status)
+      }
+    }
+    req.open('GET', requestRL, true);
+    req.send();
+    req.onreadystatechange = processRequest;
+  }).then(cb)
+}
+function RenderList(selector, html) {
+  let parent = document.querySelector(selector);
+  $parent.innerHTML = html
+}
 
+function GetHtmlRepoList(reposatories) {
+  return reposatories.map(repo => {
+    return `<li><a href="${repo.url}">${repo.name}</a></li>`
+  }).join('')
+}
+
+function GetHtmlMemberList(members) {
+  return members.map(member => {
+    return `<li><a href="${member.url}"><img =src${member.avatar_url} width="150">${repo.name}</a></li>`
+  }).join('')
+}
+const members = []
 const GitHubORG = 'HackYourFuture'
 const HYFReposApiEndpoint = `https://api.github.com/orgs/HackYourFuture/repos`
 const HYFMembersApiEndpiont = `https://api.github.com/orgs/${GitHubORG}/members`
- 
+
 let $repoList = '.repo-list ul'
 let $memberList = '.member-list ul'
 
